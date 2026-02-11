694d85b071f6024ecbea0119a0b3ebc3ca5e2b7e
n00ra666 <noora.kojonen@tuni.com>
2026-02-11 12:58:01 +0200

    Character controller pt. 1

---
+++ Code/CharacterController.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+
+public partial class CharacterController : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+	private float _horizontalMovement = 0;
+	private bool _isJumping = false;
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("Jump"))
+		{
+			_isJumping = true;
+		}
+	}
+
+    public override void _Process(double delta)
+    {
+        _horizontalMovement = Input.GetAxis("MoveLeft", "MoveRight");
+    }
+
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (_isJumping)
+		{
+			GD.Print("Jump pressed");
+			_isJumping = false;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		if (!Mathf.IsZeroApprox(_horizontalMovement))
+		{
+			GD.Print(_horizontalMovement * Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
